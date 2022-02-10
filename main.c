adcc584ffd05abf8875bc92087136e3c46dde7aa
Nate Schutte <natekschutte@gmail.com>
2022-02-10 14:48:51 -0500

    update main.c to remove old hw includes

--- main.c
+++ main.c
@@ -1,19 +1,10 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <math.h>
-#include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 #include "driverlib/adc.h"
 #include "driverlib/timer.h"
-
-
-
-
-
-
-
 
 
 #include "fix_fft.h"
