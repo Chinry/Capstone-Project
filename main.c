e3251ddab0c59d8526f8b1b54996402792361e9e
Nate Schutte <natekschutte@gmail.com>
2022-02-10 14:56:04 -0500

    oops, need inc/

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <math.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 #include "driverlib/adc.h"
